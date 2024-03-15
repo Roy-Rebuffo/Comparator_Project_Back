97ff77ebf6cbcedd2061bec5b2aab7b8d1c665c8
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2024-03-15 21:13:17 +0100

    lol

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-
-const DB_URL = process.env.DB_URL;
-=======
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 dotenv.config();
->>>>>>> 0e701c9c0c41daf758bccf573908bd30cabf7e0e
 
 
-<<<<<<< HEAD
-        console.log(`conectado a ${name}DB en host ${host}`);
-    } catch (error) {
-        console.log('error conectando a nuestra BBDD', error);
-    }
-}
-module.exports = {connect};
-=======
 const urlDB = process.env.MONGO_DB
 const mongoDb = urlDB;
 
@@ -35,4 +21,3 @@ const connect = async () => {
 };
 
 module.exports = { connect };
->>>>>>> 0e701c9c0c41daf758bccf573908bd30cabf7e0e
