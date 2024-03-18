a911d204c2c1059ed2332d6beb3c0e433a540bdd
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2024-03-18 12:13:58 +0100

    feat new /

--- src/api/routes/scraping.routes.js
+++ src/api/routes/scraping.routes.js
@@ -1,7 +1,7 @@
 const ScrapingRoutes = require('express').Router();
 const { getData, getDataAhorramas } = require('../contollers/scraping.controllers');
 
-ScrapingRoutes.get('/scrapedata', getData);
+ScrapingRoutes.get('/scrapedatacarrefour', getData);
 ScrapingRoutes.get('/scrapedataahorramas', getDataAhorramas);
 
 module.exports = ScrapingRoutes;
